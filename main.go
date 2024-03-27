a159928e2833a2b2b587d14bf186b3c3b6df56cc
Yacine Aridhi <77867568+aridhi-yacine@users.noreply.github.com>
2024-03-27 18:38:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -11,16 +11,16 @@ func main() {
 	for i := 0; i <= 100; i++ {
 
 		if i%3 == 0 && i%5 == 0 {
-			// check if the i is a common multiple of 3 and 5 and print fizz buzz
+			// check if the i is a common multiple of 3 and 5 print fizz buzz
 			fmt.Println(i, " Fizz Buzz")
 		} else if i%3 == 0 {
-			// check if the i is a multiple of 3 and print fizz
+			// check if the i is a multiple of 3 print fizz
 			fmt.Println(i, " Fizz")
 		} else if i%5 == 0 {
-			// check if the i is a multiple of 5 and print buzz
+			// check if the i is a multiple of 5 print buzz
 			fmt.Println(i, " Buzz")
 		} else {
-			//	print numbers i that are neither multiples of 3 nor 5
+			// print numbers i that are neither multiples of 3 nor 5
 			fmt.Println(i)
 		}
 
